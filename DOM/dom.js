e2f46bc5084b053ef073cc97daf78eb4785be694
Ezequiel Bamio <ezequielbamiok@gmail.com>
2022-02-05 09:44:28 -0300

    DOM

---
+++ DOM/dom.js
@@ -0,0 +1,46 @@
+// # Web APIs
+// console.log(window);
+// console.log(document);
+
+// speechSynthesis.speak(new SpeechSynthesisUtterance('My name is Ezequiel and I am studying programming.'));
+
+// # DOM
+
+// console.log('******* Elementos del DOM *******');
+// console.log(document);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.doctype);
+// console.log(document.characterSet);
+// console.log(document.title);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.forms);
+// console.log(document.styleSheets);
+// console.log(document.scripts);
+// setTimeout(() => {
+//     console.log(document.getSelection().toString());
+// }, 3000);
+// document.write('<h2>Hello World from the DOM</h2>');
+
+// console.log('******* Manejo del DOM *******');
+
+// //No se usan
+// console.log(document.getElementsByTagName('li'));
+// console.log(document.getElementsByClassName('card'));
+// console.log(document.getElementsByName('nombre'));
+// //
+// console.log(document.getElementById('menu'));
+// console.log(document.querySelector('#menu'));
+// console.log(document.querySelector('a'));
+// console.log(document.querySelectorAll('a').length);
+// document.querySelectorAll('a').forEach( ( e ) => console.log(e));
+// console.log(document.querySelector('.card'));
+// console.log(document.querySelectorAll('.card')[2]);
+// console.log(document.querySelector('#menu li'));
+// console.log(document.querySelectorAll('#menu li'));
+
+
+
+

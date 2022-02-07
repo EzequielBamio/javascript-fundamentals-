ddd85a109dffabd164359a8df8a8abec4a19b484
Ezequiel Bamio <ezequielbamiok@gmail.com>
2022-02-07 09:27:12 -0300

    DOM: Class CSS, Text and HTML, DOM Traversing.

--- DOM/dom.js
+++ DOM/dom.js
@@ -76,44 +76,105 @@
 // $linkDOM.removeAttribute('data-id');
 // console.log($linkDOM.hasAttribute('data-id'));
 
-console.log('******* Estilos y variables en CSS *******');
+// console.log('******* Estilos y variables en CSS *******');
 
-const $linkDOM = document.querySelector('.link-dom');
+// const $linkDOM = document.querySelector('.link-dom');
 
-console.log($linkDOM.style);
-console.log($linkDOM.getAttribute('style'));
-console.log($linkDOM.style.backgroundColor);
-console.log($linkDOM.style.color);
-console.log(window.getComputedStyle($linkDOM));
-console.log(window.getComputedStyle($linkDOM).getPropertyValue('color'));
+// console.log($linkDOM.style);
+// console.log($linkDOM.getAttribute('style'));
+// console.log($linkDOM.style.backgroundColor);
+// console.log($linkDOM.style.color);
+// console.log(window.getComputedStyle($linkDOM));
+// console.log(window.getComputedStyle($linkDOM).getPropertyValue('color'));
 
-$linkDOM.style.setProperty('text-decoration', 'none');
-$linkDOM.style.setProperty('display', 'block');
-$linkDOM.style.setProperty('text-align', 'center');
-$linkDOM.style.width = '50%;';
-$linkDOM.style.marginLeft = 'auto';
-$linkDOM.style.marginRight = 'auto';
-$linkDOM.style.padding = '1rem';
-$linkDOM.style.borderRadius = '0.5rem';
+// $linkDOM.style.setProperty('text-decoration', 'none');
+// $linkDOM.style.setProperty('display', 'block');
+// $linkDOM.style.setProperty('text-align', 'center');
+// $linkDOM.style.width = '50%;';
+// $linkDOM.style.marginLeft = 'auto';
+// $linkDOM.style.marginRight = 'auto';
+// $linkDOM.style.padding = '1rem';
+// $linkDOM.style.borderRadius = '0.5rem';
 
-console.log($linkDOM.style);
-console.log($linkDOM.getAttribute('style'));
-console.log(window.getComputedStyle($linkDOM));
+// console.log($linkDOM.style);
+// console.log($linkDOM.getAttribute('style'));
+// console.log(window.getComputedStyle($linkDOM));
 
-//Variables CSS - Custom Properties CSS
-const $html = document.documentElement,
-      $body = document.body;
+// //Variables CSS - Custom Properties CSS
+// const $html = document.documentElement,
+//       $body = document.body;
 
-let varDarkColor = window.getComputedStyle($html).getPropertyValue('--dark-color'),
-varYelloColor = window.getComputedStyle($html).getPropertyValue('--yellow-color');
+// let varDarkColor = window.getComputedStyle($html).getPropertyValue('--dark-color'),
+// varYelloColor = window.getComputedStyle($html).getPropertyValue('--yellow-color');
       
-console.log(varDarkColor, varYelloColor);
+// console.log(varDarkColor, varYelloColor);
 
-$body.style.backgroundColor = varDarkColor;
-$body.style.color = varYelloColor;
+// $body.style.backgroundColor = varDarkColor;
+// $body.style.color = varYelloColor;
 
-$html.style.setProperty('--dark-color', '#000');
-varDarkColor = window.getComputedStyle($html).getPropertyValue('--dark-color');
+// $html.style.setProperty('--dark-color', '#000');
+// varDarkColor = window.getComputedStyle($html).getPropertyValue('--dark-color');
 
-$body.style.setProperty('background-color', varDarkColor);
+// $body.style.setProperty('background-color', varDarkColor);
+
+// console.log('******* Clases en CSS *******');
+
+// const $card = document.querySelector('.card');
+
+// console.log($card);
+// console.log($card.className);
+// console.log($card.classList);
+// console.log($card.classList.contains('rotate-45'));
+// $card.classList.add('rotate-45');
+// console.log($card.className);
+// console.log($card.classList);
+// $card.classList.remove('rotate-45');
+// console.log($card.classList.contains('rotate-45'));
+// $card.classList.toggle('rotate-45');
+// console.log($card.classList.contains('rotate-45'));
+// $card.classList.toggle('rotate-45');
+// console.log($card.classList.contains('rotate-45'));
+// $card.classList.toggle('rotate-45');
+// $card.classList.replace('rotate-45', 'rotate-135');
+// $card.classList.add('opacity-80', 'sepia');
+// $card.classList.remove('opacity-80', 'sepia');
+// $card.classList.toggle('opacity-80', 'sepia');
+
+// console.log('******* Texto y HTML *******');
+
+// const $whatIsDom = document.getElementById('que-es');
+// let text = `
+//     <p>
+//     El modelo de Objetos del Documento (<b><i> DOM - Document Object Model </i></b>) es una API para documentos en HTML y XML.
+//     </p>
+//     <p>
+//     Este provee una representacion estructural del documento, permitiendo modificar su contenido y presentacion visual mediante codigo JS.
+//     </p>
+//     <p>
+//     <mark>El DOM no es parte de la especificación de JavaScript, es una API para los navegadores.</mark>
+//     </p>
+// `;
+
+// console.log($whatIsDom);
+
+// // $whatIsDom.innerText = text;
+// $whatIsDom.textContent = text;
+// $whatIsDom.innerHTML = text;
+// $whatIsDom.outerHTML = text;
+
+// console.log('******* Recorriendo el DOM *******');
+
+// const $cards = document.querySelector('.cards');
+
+// console.log($cards);
+// console.log($cards.children);
+// console.log($cards.children[2]);
+// console.log($cards.parentElement);
+// console.log($cards.firstElementChild);
+// console.log($cards.lastElementChild);
+// console.log($cards.previousElementSibling);
+// console.log($cards.nextElementSibling);
+// console.log($cards.closest('div'));
+// console.log($cards.closest('body'));
+// console.log($cards.children[3].closest('section'));
 

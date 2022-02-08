5081321d62c7a7befdec4d2767b94ae09c092bba
Ezequiel Bamio <ezequielbamiok@gmail.com>
2022-02-08 09:24:17 -0300

    DOM: Elements and Fragment, Templates HTML, Modify elements.

--- DOM/dom.js
+++ DOM/dom.js
@@ -178,3 +178,134 @@
 // console.log($cards.closest('body'));
 // console.log($cards.children[3].closest('section'));
 
+// console.log('******* Creando elementos y fragmentos *******');
+
+// const $figure = document.createElement('figure'),
+//       $img = document.createElement('img'),
+//       $figcaption = document.createElement('figcaption'),
+//       $figcaptionText = document.createTextNode('Animals'),
+//       $cards = document.querySelector('.cards'),
+//       $figure2 = document.createElement('figure');
+
+// $img.setAttribute('src', 'https://placeimg.com/200/200/animals');
+// $img.setAttribute('alt', 'Animals');
+// $figure.classList.add('card')
+
+// $figcaption.appendChild($figcaptionText);
+// $figure.appendChild($img);
+// $figure.appendChild($figcaption);
+// $cards.appendChild($figure);
+
+// $figure2.innerHTML = `
+//  <img src="https://placeimg.com/200/200/people" alt="People">
+//  <figcaption>People</figcaption>
+// `;
+
+// $figure2.classList.add('card');
+// $cards.appendChild($figure2);
+
+// const estaciones = ['Primavera', 'Verano', 'Otoño', 'Invierno'],
+//       $ul = document.createElement('ul');
+
+// document.write('<h3>Estaciones del Año</h3>');
+
+// document.body.appendChild($ul);
+
+// estaciones.forEach( ( el ) => {
+//     const $li = document.createElement('li');
+//     $li.textContent = el;
+//     $ul.appendChild($li);
+// });
+
+// const continentes = ['Africa', 'America', 'Asia', 'Europa', 'Oceania'],
+//       $ul2 = document.createElement('ul');
+
+// document.write('<h3>Continentes del Mundo</h3>');
+// document.body.appendChild($ul2);
+// $ul2.innerHTML = '';
+// continentes.forEach( (el) => $ul2.innerHTML += `<li>${el}</li>`);
+
+// const meses = [
+//     'Enero',
+//     'Febrero',
+//     'Marzo',
+//     'Abril',
+//     'Mayo',
+//     'Junio',
+//     'Julio',
+//     'Agosto',
+//     'Septiembre',
+//     'Octubre',
+//     'Noviembre',
+//     'Diciembre',
+// ],
+//      $ul3 = document.createElement('ul'),
+//      $fragment = document.createDocumentFragment();
+
+// meses.forEach( (el) => {
+//     const $li = document.createElement('li');
+//     $li.textContent = el;
+//     $fragment.appendChild($li);
+// });
+
+// document.write('<h3>Meses del año</h3>');
+// $ul3.appendChild($fragment);
+// document.body.appendChild($ul3);
+
+// console.log('******* Templates HTML *******');
+
+// const $cards = document.querySelector('.cards'),
+//       $template = document.getElementById('template-card').content,
+//       $fragment = document.createDocumentFragment(),
+//       cardContent = [
+//           {
+//               title: 'Tecnologia',
+//               img: 'https://placeimg.com/200/200/tech',
+//           },
+//           {
+//             title: 'Animals',
+//             img: 'https://placeimg.com/200/200/animals',
+//           },
+//           {
+//             title: 'Arquitectura',
+//             img: 'https://placeimg.com/200/200/arch',
+//           },
+//           {
+//             title: 'Gente',
+//             img: 'https://placeimg.com/200/200/people',
+//           },
+//           {
+//             title: 'Naturaleza',
+//             img: 'https://placeimg.com/200/200/nature',
+//           },
+//       ];
+
+// cardContent.forEach( (el) => {
+//     $template.querySelector('img').setAttribute('src', el.img);
+//     $template.querySelector('img').setAttribute('alt', el.title);
+//     $template.querySelector('figcaption').textContent = el.title;
+
+//     let $clone = document.importNode($template, true);
+//     $fragment.appendChild($clone);
+// });
+
+// $cards.appendChild($fragment);
+
+// console.log('******* Modificando Elementos (Old Style) *******');
+
+// const $cards = document.querySelector('.cards'),
+//       $newCard = document.createElement('figure'),
+//       $cloneCards = $cards.cloneNode(true);
+
+// $newCard.innerHTML = `
+//     <img src="https://placeimg.com/200/200/any" alt="Any">
+//     <figcaption>Any</figcaption>
+// `;
+
+// $newCard.classList.add('card');
+
+// // $cards.replaceChild($newCard, $cards.children[2]);
+// // $cards.removeChild($cards.lastElementChild);
+// // $cards.insertBefore($newCard, $cards.firstChild);
+
+// document.body.appendChild($cloneCards);

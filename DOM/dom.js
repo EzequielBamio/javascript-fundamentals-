cd17c0d104a51d1e56a702700b789f490d6a0219
Ezequiel Bamio <ezequielbamiok@gmail.com>
2022-02-12 11:13:46 -0300

    DOM Complete

--- DOM/dom.js
+++ DOM/dom.js
@@ -491,26 +491,67 @@ Posiciones:
 //     console.log(e);
 // });
 
-console.log('******* Metodos *******');
-// window.alert('Alerta');
-// window.confirm('Confirmación');
-// window.prompt('Aviso');
+// console.log('******* Metodos *******');
+// // window.alert('Alerta');
+// // window.confirm('Confirmación');
+// // window.prompt('Aviso');
 
-const $btnAbrir = document.getElementById('abrir-ventana'),
-      $btnCerrar = document.getElementById('cerrar-ventana'),
-      $btnImprimir = document.getElementById('imprimir-ventana');
+// const $btnAbrir = document.getElementById('abrir-ventana'),
+//       $btnCerrar = document.getElementById('cerrar-ventana'),
+//       $btnImprimir = document.getElementById('imprimir-ventana');
 
-let ventana;
+// let ventana;
 
-$btnAbrir.addEventListener('click', (e) => {
-    ventana = window.open('https://google.com/');
-});
+// $btnAbrir.addEventListener('click', (e) => {
+//     ventana = window.open('https://google.com/');
+// });
 
-$btnCerrar.addEventListener('click', (e) => {
-    // window.close();
-    ventana.close();;
-});
+// $btnCerrar.addEventListener('click', (e) => {
+//     // window.close();
+//     ventana.close();;
+// });
 
-$btnImprimir.addEventListener('click', (e) => {
-    window.print();
-});
+// $btnImprimir.addEventListener('click', (e) => {
+//     window.print();
+// });
+
+console.log('******* Objeto URL (location) *******');
+
+// console.log(location);
+// console.log(location.origin);
+// console.log(location.protocol);
+// console.log(location.host);
+// console.log(location.hostname);
+// console.log(location.port);
+// console.log(location.href);
+// console.log(location.hasf);
+// console.log(location.hash);
+// console.log(location.search);
+// console.log(location.pathname);
+// location.reload();
+
+// console.log(history);
+// console.log(history.length);
+// console.log(history.scrollRestoration);
+// console.log(history.state);
+// history.back();
+// history.back(2);
+// history.forward();
+// history.forward(2);
+// history.go(3);
+// history.go(0);
+// history.go(-1);
+
+// console.log(navigator);
+// console.log(navigator.connection);
+// console.log(navigator.geolocation);
+// console.log(navigator.mediaDevices);
+// console.log(navigator.mimeTypes);
+// console.log(navigator.onLine);
+// console.log(navigator.serviceWorker);
+// console.log(navigator.storage);
+// console.log(navigator.usb);
+// console.log(navigator.userAgent);
+
+
+

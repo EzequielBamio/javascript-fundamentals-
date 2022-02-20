84ce6376d2a236326c2911ceb73bfe17e6096131
Ezequiel Bamio <ezequielbamiok@gmail.com>
2022-02-20 09:03:16 -0300

    DOM Exercise 4

---
+++ Ejercicios-DOM/js/cuenta_regresiva.js
@@ -0,0 +1,26 @@
+export function countdown(id, limitDate, message) {
+    const $countdown = document.getElementById(id),
+          countdownDate = new Date(limitDate).getTime();
+    // let timeCurrent = new Date();
+    // let years = time.getFullYear() - timeCurrent.getFullYear();
+    
+    
+    
+    let countdownTempo = setInterval(() => {
+        let now = new Date().getTime(),
+            limitTime = countdownDate - now,
+            days = Math.floor(limitTime / (1000 * 60 * 60 * 24)),
+            hours = ("0" + Math.floor(limitTime % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))).slice(-2),
+            minutes = ("0" + Math.floor(limitTime % (1000 * 60 * 60) / (1000 * 60))).slice(-2),
+            seconds = ("0" + Math.floor(limitTime % (1000 * 60) / 1000)).slice(-2);
+
+
+        $countdown.innerHTML = `<h3>Faltan: ${days} dias ${hours} horas ${minutes} minutos ${seconds} segundos.</h3>`;  
+
+        if(limitTime < 0) {
+            clearInterval(countdownTempo);
+            $countdown.innerHTML = `<h3>${message}</h3>`;
+        }
+
+    }, 1000);
+}
--- Ejercicios-DOM/js/menu_dom.js
+++ Ejercicios-DOM/js/menu_dom.js
@@ -1,6 +1,7 @@
 import { alarm, digitalClock } from "./reloj.js";
 import hamburgerMenu from "./menu_hamburger.js";
 import { moveBall, shortcuts } from "./teclado.js";
+import { countdown } from "./cuenta_regresiva.js";
 
 const d = document;
 
@@ -8,6 +9,7 @@ d.addEventListener('DOMContentLoaded', (e) => {
     hamburgerMenu('.panel-btn', '.panel', '.menu a');
     digitalClock('#reloj', '#reloj-activar', '#reloj-desactivar');
     alarm('./assets/alarma.mp3', '#alarma-activar', '#alarma-desactivar');
+    countdown('countdown', 'Apr 14, 2022 08:30:00', 'Feliz Cumpleaño, Ezequiel Bamio!');
 });
 
 d.addEventListener('keydown', (e) => {

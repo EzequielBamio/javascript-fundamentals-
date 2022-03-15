88690e9b9e8943828705279770bec80054129c16
Ezequiel Bamio <ezequielbamiok@gmail.com>
2022-03-15 09:02:38 -0300

    API Rest: Crud Fetch

--- crud-ajax/js/crud-ajax.js
+++ crud-ajax/js/crud-ajax.js
@@ -31,14 +31,14 @@ const getAll = () => {
         // method: 'GET',
         url: 'http://localhost:5555/playersArg',
         success: (res) => {
-            console.log(res);
+            // console.log(res);
             res.forEach( (el) => {
                 $template.querySelector('.name').textContent = el.name;
                 $template.querySelector('.number').textContent = el.number;
-                $template.querySelector('edit').dataset.id = el.id;
-                $template.querySelector('edit').dataset.name = el.name;
-                $template.querySelector('edit').dataset.number = el.number;
-                $template.querySelector('delete').dataset.id = el.id;
+                $template.querySelector('.edit').dataset.id = el.id;
+                $template.querySelector('.edit').dataset.name = el.name;
+                $template.querySelector('.edit').dataset.number = el.number;
+                $template.querySelector('.delete').dataset.id = el.id;
 
             
                 let $clone = d.importNode($template, true);
@@ -58,3 +58,63 @@ const getAll = () => {
 }
 
 d.addEventListener('DOMContentLoaded', getAll);
+
+d.addEventListener('submit', (e) => {
+    if(e.target === $form) {
+        e.preventDefault();
+
+        if(!e.target.id.value) {
+            //CREATE - POST
+            ajax({
+                url: 'http://localhost:5555/playersArg',
+                method: 'POST',
+                success: (res) => location.reload,
+                error: (err) => $table.insertAdjacentHTML('afterend', `<p><b>${err}</b></p>`),
+                data: {
+                    name: e.target.name.value,
+                    number: e.target.number.value,
+                }
+            });
+        } else {
+            //UPDATE -PUT
+            ajax({
+                url: `http://localhost:5555/playersArg/${e.target.id.value}`,
+                method: 'PUT',
+                success: (res) => location.reload,
+                error: (err) => $table.insertAdjacentHTML('afterend', `<p><b>${err}</b></p>`),
+                data: {
+                    name: e.target.name.value,
+                    number: e.target.number.value,
+                }
+            });
+        }
+    }
+});
+
+d.addEventListener('click', (e) => {
+
+    if(e.target.matches('.edit')){
+        $title.textContent = 'Editar Jugador';
+        $form.name.value = e.target.dataset.name;
+        $form.number.value = e.target.dataset.number;
+        $form.id.value = e.target.dataset.id;
+    }
+
+});
+
+d.addEventListener('click', (e) => {
+
+    if(e.target.matches('.delete')){
+        let isDelete = confirm(`Estas seguro de eliminar el id ${e.target.dataset.id}`);
+
+        if(isDelete) {
+            ajax({
+                url: `http://localhost:5555/playersArg/${e.target.dataset.id}`,
+                method: 'DELETE',
+                success: (res) => location.reload,
+                error: (err) => alert(err),
+            });
+        }
+    }
+
+});
--- crud-fetch/js/crud-fetch.js
+++ crud-fetch/js/crud-fetch.js
@@ -4,3 +4,33 @@ const d = document,
       $title = d.querySelector('.crud-title'),
       $template = d.querySelector('.crud-template').content,
       $fragment = d.createDocumentFragment();
+
+const getAll = async () => {
+    try{
+        let res = await fetch('http://localhost:5555/playersArg'),
+            json = await res.json();
+
+        if(!res.ok) { throw {status: res.status, statusText: res.statusText} }
+
+        json.forEach( (el) => {
+            $template.querySelector('.name').textContent = el.name;
+            $template.querySelector('.number').textContent = el.number;
+            $template.querySelector('.edit').dataset.id = el.id;
+            $template.querySelector('.edit').dataset.name = el.name;
+            $template.querySelector('.edit').dataset.number = el.number;
+            $template.querySelector('.delete').dataset.id = el.id;
+
+        
+            let $clone = d.importNode($template, true);
+            $fragment.appendChild($clone); 
+        });
+
+        $table.querySelector('tbody').appendChild($fragment);
+
+    }catch(err) {
+        let message = err.statusText || 'Ocurrio un error';
+        $table.insertAdjacentHTML('afterend', `<p><b>Error ${err.status}: ${message}</b></p>`);
+    }
+}
+
+d.addEventListener('DOMContentLoaded', getAll);

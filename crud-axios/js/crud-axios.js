a03c36e761a27b2d0ec714417e1f88a10945d6b2
Ezequiel Bamio <ezequielbamiok@gmail.com>
2022-03-16 15:28:09 -0300

    API Rest: crud axios

--- crud-axios/js/crud-axios.js
+++ crud-axios/js/crud-axios.js
@@ -32,3 +32,91 @@ const getAll = async () => {
 }
 
 d.addEventListener('DOMContentLoaded', getAll);
+
+d.addEventListener('submit', async (e) => {
+
+    if(e.target === $form) {
+        e.preventDefault();
+
+        if(!e.target.id.value) {
+            //Create - POST
+            try {
+                let options = {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json; charset=utf-8'
+                    },
+                    data: JSON.stringify({
+                        name: e.target.name.value,
+                        number: e.target.number.value
+                    })
+                },
+                    res = await axios('http://localhost:5555/playersArg', options),
+                    json = await res.data;
+
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || 'Ocurrio un error';
+                $form.insertAdjacentHTML('afterend', `<p><b>Error ${err.status}: ${message}</b></p>`);
+            }
+        } else {
+            //Update - PUT
+            try {
+                let options = {
+                    method: 'PUT',
+                    headers: {
+                        'Content-type': 'application/json; charset=utf-8'
+                    },
+                    data: JSON.stringify({
+                        name: e.target.name.value,
+                        number: e.target.number.value
+                    })
+                },
+                    res = await axios(`http://localhost:5555/playersArg/${e.target.id.value}`, options),
+                    json = await res.data;
+                    
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || 'Ocurrio un error';
+                $form.insertAdjacentHTML('afterend', `<p><b>Error ${err.status}: ${message}</b></p>`);
+            }
+        }
+
+    }
+});
+
+d.addEventListener('click', (e) => {
+
+    if(e.target.matches('.edit')){
+        $title.textContent = 'Editar Jugador';
+        $form.name.value = e.target.dataset.name;
+        $form.number.value = e.target.dataset.number;
+        $form.id.value = e.target.dataset.id;
+    }
+});
+
+d.addEventListener('click', async (e) => {
+
+    if(e.target.matches('.delete')){
+        let isDelete = confirm(`Estas seguro de eliminar el id ${e.target.dataset.id}`);
+
+        if(isDelete) {
+            try {
+                let options = {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-type': 'application/json; charset=utf-8'
+                    }
+                },
+                    res = await axios(`http://localhost:5555/playersArg/${e.target.dataset.id}`, options),
+                    json = await res.data;
+                
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || 'Ocurrio un error';
+                alert(`Error ${err.status}: ${message}`);
+            }
+        }
+    }
+
+});

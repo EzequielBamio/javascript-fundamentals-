b03ea9a8062c7222fa79c8e70cc3588fd254d351
Ezequiel Bamio <ezequielbamiok@gmail.com>
2022-03-16 09:30:13 -0300

    API Rest: crud axios

--- crud-axios/js/crud-axios.js
+++ crud-axios/js/crud-axios.js
@@ -4,3 +4,31 @@ const d = document,
       $title = d.querySelector('.crud-title'),
       $template = d.querySelector('.crud-template').content,
       $fragment = d.createDocumentFragment();
+
+const getAll = async () => {
+    try {
+        let res = await axios.get('http://localhost:5555/playersArg'),
+            json = await res.data;
+
+            json.forEach( (el) => {
+                $template.querySelector('.name').textContent = el.name;
+                $template.querySelector('.number').textContent = el.number;
+                $template.querySelector('.edit').dataset.id = el.id;
+                $template.querySelector('.edit').dataset.name = el.name;
+                $template.querySelector('.edit').dataset.number = el.number;
+                $template.querySelector('.delete').dataset.id = el.id;
+    
+            
+                let $clone = d.importNode($template, true);
+                $fragment.appendChild($clone); 
+            });
+    
+            $table.querySelector('tbody').appendChild($fragment);
+
+    } catch(err) {
+        let message = err.statusText || 'Ocurrio un error';
+        $table.insertAdjacentHTML('afterend', `<p><b>Error ${err.status}: ${message}</b></p>`);
+    }
+}
+
+d.addEventListener('DOMContentLoaded', getAll);

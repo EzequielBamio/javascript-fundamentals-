4a781c5e1382724431e069589c7599773b1f9b4a
Ezequiel Bamio <ezequielbamiok@gmail.com>
2022-03-16 09:17:40 -0300

    API Rest: crud fetch

--- crud-fetch/js/crud-fetch.js
+++ crud-fetch/js/crud-fetch.js
@@ -34,3 +34,97 @@ const getAll = async () => {
 }
 
 d.addEventListener('DOMContentLoaded', getAll);
+
+d.addEventListener('submit', async (e) => {
+    if(e.target === $form) {
+        e.preventDefault();
+
+        if(!e.target.id.value) {
+            //Create - POST
+            try {
+                let options = {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json; charset=utf-8'
+                    },
+                    body: JSON.stringify({
+                        name: e.target.name.value,
+                        number: e.target.number.value
+                    })
+                },
+                    res = await fetch('http://localhost:5555/playersArg', options),
+                    json = await res.json();
+                    
+                if(!res.ok) { throw {status: res.status, statusText: res.statusText} }
+                
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || 'Ocurrio un error';
+                $form.insertAdjacentHTML('afterend', `<p><b>Error ${err.status}: ${message}</b></p>`);
+            }
+
+        } else {
+            //Update - PUT
+            try {
+                let options = {
+                    method: 'PUT',
+                    headers: {
+                        'Content-type': 'application/json; charset=utf-8'
+                    },
+                    body: JSON.stringify({
+                        name: e.target.name.value,
+                        number: e.target.number.value
+                    })
+                },
+                    res = await fetch(`http://localhost:5555/playersArg/${e.target.id.value}`, options),
+                    json = await res.json();
+                    
+                if(!res.ok) { throw {status: res.status, statusText: res.statusText} }
+                
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || 'Ocurrio un error';
+                $form.insertAdjacentHTML('afterend', `<p><b>Error ${err.status}: ${message}</b></p>`);
+            }
+        }
+    }
+});
+
+
+d.addEventListener('click', (e) => {
+
+    if(e.target.matches('.edit')){
+        $title.textContent = 'Editar Jugador';
+        $form.name.value = e.target.dataset.name;
+        $form.number.value = e.target.dataset.number;
+        $form.id.value = e.target.dataset.id;
+    }
+});
+
+d.addEventListener('click', async (e) => {
+
+    if(e.target.matches('.delete')){
+        let isDelete = confirm(`Estas seguro de eliminar el id ${e.target.dataset.id}`);
+
+        if(isDelete) {
+            try {
+                let options = {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-type': 'application/json; charset=utf-8'
+                    }
+                },
+                    res = await fetch(`http://localhost:5555/playersArg/${e.target.dataset.id}`, options),
+                    json = await res.json();
+                    
+                if(!res.ok) { throw {status: res.status, statusText: res.statusText} }
+                
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || 'Ocurrio un error';
+                alert(`Error ${err.status}: ${message}`);
+            }
+        }
+    }
+
+});
